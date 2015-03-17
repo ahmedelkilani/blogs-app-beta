e18020ae57b1f0260f53339a54be7b636bf11521
Ahmed El-Kilani <ahmedelkilani@gmail.com>
2015-03-17 17:12:56 +0200

    fix jshint warning

--- app/js/components/_modules.js
+++ app/js/components/_modules.js
@@ -14,6 +14,6 @@
 
     // version component module
     blogsApp.components.version =
-        angular.module('blogsApp.components.version', [])
+        angular.module('blogsApp.components.version', []);
 
 }());
