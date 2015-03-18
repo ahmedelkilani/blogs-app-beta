10c679e4f797552d8c414cf07401dd5cf55cae39
Ahmed El-Kilani <ahmedelkilani@gmail.com>
2015-03-18 22:59:46 +0200

    added postsServiceSpec

---
+++ app/js/tests/unit/services/posts/postsServiceSpec.js
@@ -0,0 +1,47 @@
+;
+(function () {
+    'use strict';
+
+    ddescribe('blogsApp.services.posts module', function () {
+
+        beforeEach(module('blogsApp.settings'));
+        beforeEach(module('blogsApp.services.posts'));
+
+        describe('postsService service', function () {
+            var postsService;
+
+            // Mocking
+
+            beforeEach(function () {
+                module(function ($provide) {
+                    $provide.service('appResource', function () {
+                        return {
+                            resource: function () {
+                                return {
+                                    query: function () {
+                                        return [{'author': 'Ahmed'}];
+                                    }
+                                };
+                            }
+                        };
+                    });
+                });
+            });
+
+            // Injectors
+
+            beforeEach(function () {
+                inject(function ($injector) {
+                    postsService = $injector.get('postsService');
+                });
+            });
+
+            it('should get a list of blog posts', function () {
+
+                expect(postsService.getPosts()[0].author).toBe('Ahmed');
+
+            });
+
+        });
+    });
+}());
